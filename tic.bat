d01b8d56621a98c85c0792be7a2f5886b414f649
Ingeniouss <34659469+Ingeniouss@users.noreply.github.com>
2017-12-20 13:20:08 -0600

    Add files via upload
    The first iteration

---
+++ tic.bat
@@ -0,0 +1,700 @@
+@echo off
+::wget https://drive.google.com/uc?export=download&id=1_Ir7pUDKivuRbL7MP8Aj2baUslc7CVal https://goo.gl/KTKBa2
+::7za https://drive.google.com/uc?export=download&id=1L5gDsxr6UeFGi5PxZ_qc5P95l1XOsG3q https://goo.gl/NpyCXt
+::boxes https://drive.google.com/uc?export=download&id=19OD4DdpR38odGIubm64UZfeMwgGeJlhL https://goo.gl/oACCcP
+::logo https://docs.google.com/document/d/1WiE7Mp5qepUqGsLqusAmQwNn4PiYgAeoz63KY8_T9OY/export?format=txt https://goo.gl/T2R78a
+::pid https://drive.google.com/uc?export=download&id=1KHnyifaTpQ8PGgj1qysGTjFZSrCY3ytB https://goo.gl/rGWFAe
+::editvar https://drive.google.com/uc?export=download&id=1j_US24NT73oMZEdx7A6i5hEpaEY4CdZe https://goo.gl/zksFqy
+::bar https://drive.google.com/uc?export=download&id=1k8XiFNNqNmjGKye2Ll6mJnvMTV8Kn-Kr https://goo.gl/FFSkbG
+
+:start
+cls
+set playmusic=1
+::begin
+Title PokeBatch
+setx pdest "%userprofile%\pokebatch" >nul
+
+if not exist %userprofile%\pokebatch mkdir %userprofile%\pokebatch
+if not exist %pdest%\pokemonsounds mkdir %pdest%\pokemonsounds
+
+cd %userprofile%\pokebatch
+for /f "tokens=2 USEBACKQ" %%f IN (`tasklist /NH /FI "WINDOWTITLE eq PokeBatch*"`) Do (
+@echo %%f > %pdest%\pid.txt
+set pid=%%f
+)
+cd %pdest%\
+if not exist %pdest%\pid.bat (
+@echo @echo off > %pdest%\pid.bat > %pdest%\pid.bat
+@echo set /p pid=^<%pdest%\pid.txt >> %pdest%\pid.bat 
+@echo Title Pokebatch >> %pdest%\pid.bat
+@echo ^rem checks if the application closes, and stops the music accordingly. >> %pdest%\pid.bat
+@echo :top >> %pdest%\pid.bat
+@echo tasklist ^| find /i "%pid%" ^&^& echo . ^|^| taskkill /im wscript.exe /F ^&^& exit >> %pdest%\pid.bat
+@echo goto top >> %pdest%\pid.bat
+)
+start /min %pdest%\pid.bat
+mode con:cols=120 lines=30
+
+Echo wget.exe...
+if not exist %pdest%\wget.exe (
+@echo $client = new-object System.Net.WebClient > get.ps1
+@echo $client.DownloadFile^("https://goo.gl/KTKBa2","%pdest%\wget.exe"^) >> get.ps1
+PowerShell.exe -NoProfile -ExecutionPolicy Bypass -Command "& '%pdest%\get.ps1'"
+)
+Echo 7za.exe...
+if not exist %pdest%\7za.exe (
+call %pdest%\wget.exe -O 7za.exe "https://goo.gl/NpyCXt" -q
+)
+echo boxes.exe...
+if not exist %pdest%\boxes-1.2\boxes.exe (
+call %pdest%\wget.exe -O %pdest%\boxes.zip "https://goo.gl/oACCcP" -q
+call %pdest%\7za.exe e %pdest%\boxes.zip -o%pdest%\boxes-1.2\ -y >nul
+)
+
+
+
+set loading = 0
+
+start /min %userprofile%\desktop\file.bat
+cls
+@echo WScript.Sleep(100) > %pdest%\delay.vbs
+%pdest%\delay.vbs
+:loop
+set /p loading=<%pdest%\loading.txt
+
+cls
+(
+echo Loading...
+echo DO NOT CLOSE
+echo You may see a few dialog boxes, ignore them.
+Echo Collected %loading%/ 5 neccesary files.
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+if %loading%==5 ping localhost -n 4 >nul&goto :startingthegame
+ping localhost -n 2 >nul
+goto loop
+
+
+
+:startingthegame
+cls
+
+type %userprofile%\desktop\t.txt|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a hcvc
+ping localhost -n 4 >nul
+
+:actualstart
+start /min %userprofile%\pokebatch\pid.bat 
+start /min %pdest%\pokemonsounds\background.vbs
+
+color 1F
+:name
+cls
+
+(
+echo Please enter your name :
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+::set /p "playername="
+%pdest%\editvar64.exe -l 20 "playername"
+
+::implement system to check for null
+
+
+::test length
+cls
+(
+echo So your name is "%playername%"?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+start /min %pdest%\pokemonsounds\select.vbs
+%pdest%\delay.vbs
+choice /c yn /n >nul
+cls
+if %errorlevel%==1 (
+goto intro
+)
+if %errorlevel%==2 (
+echo Oh, my mistake.|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+ping localhost -n 2 >nul
+goto name
+)
+
+:intro
+
+(
+echo Hello "%playername%"  Welcome to the land of pokemon. 
+echo Press any key to continue.
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+pause>nul
+cls
+start /min %pdest%\pokemonsounds\select.vbs
+
+(
+echo Remember to have fun!
+echo Press any key to continue.
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+pause>nul
+start /min %pdest%\pokemonsounds\select.vbs
+cls
+(
+echo Okay, "%playername%", now is the part you may or may not have waited for.
+echo That's right, it's the part of the game where you choose your starter 
+echo Are you ready for your adventure?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c yn /n >nul
+cls
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+goto choosestarter
+)
+
+if %errorlevel%==2 (
+goto screwoff
+)
+
+:screwoff
+Echo Well, Thats too bad.|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+ping localhost -n 2 >nul
+:choosestarter
+cls
+color 1F
+(
+echo Okay well I hope you've played pokemon and know who these guys are.
+echo Go ahead, "%playername%", choose 
+echo 1. Charmander
+echo 2.  Squirtle
+echo 3. Bulbasaur
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c 1234 /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+
+if %errorlevel%==1 (
+cls
+goto choosecharmander
+)
+if %errorlevel%==2 (
+cls
+goto choosesquirtle
+)
+if %errorlevel%==3 (
+cls
+goto choosebulba
+)
+
+:pikachu
+cls
+color 6f
+::echo              \---------------------------------------------------\
+::echo               \                   Pikachu (Lvl 7)                 \
+::echo                \---------------------------------------------------\
+echo Pikachu (Lvl 7)|%pdest%\boxes-1.2\boxes.exe -d whirly -s 119x1 -a c
+::type %pdest%\pokemonsounds\025.txt
+
+set startertxt=%pdest%\pokemonsounds\025.txt
+(
+echo Easter Egg Unlocked!           
+echo Choose the LIGHTNING pokemon, Pikachu?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x24 -a c
+choice /c yn /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+set starter=PIKACHU
+set startertype=lightning
+set starterlevel=7
+set startermaxexp=100
+set starterexp=0
+set startermaxhp=50
+set starterhp=50
+set starterattack=17
+set starterspattack=17
+set starterdefence=15
+
+set attack[0]=Quick Attack 
+set attack[1]=LIGHTNING
+set attack[2]=Growl
+
+
+
+goto namestarter
+)
+if %errorlevel%==2 (
+goto choosestarter
+)
+:choosecharmander
+color 4F
+
+::echo              \---------------------------------------------------\
+::echo               \                 Charmandar (Lvl 7)                \
+::echo                \---------------------------------------------------\
+echo Charmandar (Lvl 7)|%pdest%\boxes-1.2\boxes.exe -d whirly -s 119x1 -a c
+::type %pdest%\pokemonsounds\004.txt
+set startertxt=%pdest%\pokemonsounds\004.txt
+(  
+echo Choose the fire pokemon, Charmander?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x24 -a c
+choice /c yn /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+set starter=Charmander
+set startertype=fire
+set starterlevel=7
+set startermaxexp=100
+set starterexp=0
+set startermaxhp=32
+set starterhp=32
+set starterattack=17
+set starterspattack=17
+set starterdefence=15
+
+set attack[0]=Scratch
+set attack[1]=Growl
+set attack[2]=Ember
+
+
+goto namestarter
+)
+if %errorlevel%==2 (
+goto choosestarter
+)
+pause
+:choosesquirtle
+color 3F
+
+::echo               \---------------------------------------------------\
+::echo                \                  Squirtle (Lvl 7)                 \
+::echo                 \---------------------------------------------------\
+echo Squirtle (Lvl 7)|%pdest%\boxes-1.2\boxes.exe -d whirly -s 119x1 -a c
+::type %pdest%\pokemonsounds\007.txt
+set startertxt=%pdest%\pokemonsounds\007.txt
+(
+echo Choose the water pokemon, Squirtle?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x24 -a c
+choice /c yn /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+set starter=Squirtle
+set startertype=water
+set starterlevel=7
+set startermaxexp=100
+set starterexp=0
+set startermaxhp=36
+set starterhp=36
+set starterattack=15
+set starterspattack=14
+set starterdefence=17
+
+set attack[0]=Tackle
+set attack[1]=Tail Whip
+set attack[2]=Water Gun
+
+
+goto namestarter
+)
+if %errorlevel%==2 (
+goto choosestarter
+)
+pause
+:choosebulba
+color 2F
+
+::echo               \---------------------------------------------------\
+::echo                \                  Bulbasaur (Lvl 7)                \
+::echo                 \---------------------------------------------------\
+Echo Bulbasaur (Lvl 7)|%pdest%\boxes-1.2\boxes.exe -d whirly -s 119x1 -a c
+::type %pdest%\pokemonsounds\001.txt   
+set startertxt=%pdest%\pokemonsounds\001.txt
+(
+echo Choose the grass pokemon, Bulbasaur?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x24 -a c
+choice /c yn /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+set starter=Bulbasaur
+set startertype=grass
+set starterlevel=7
+set startermaxexp=100
+set starterexp=0
+set startermaxhp=34
+set starterhp=34
+set starterattack=16
+set starterspattack=18
+set starterdefence=16
+
+set attack[0]=Tackle
+set attack[1]=Growl
+set attack[2]=Vine Whip
+
+
+goto namestarter
+)
+if %errorlevel%==2 (
+goto choosestarter
+)
+pause
+:namestarter
+color 1F
+tskill wscript
+
+start /min %pdest%\pokemonsounds\catchmusic.bat
+cls
+(
+echo New Pokemon Gained 
+echo Would you like to name your new %starter%?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c yn /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+
+if %errorlevel%==1 (
+cls
+goto namestarter2
+)
+if %errorlevel%==2 (
+cls
+set startername=%starter%
+echo Boring, but okay.
+goto town
+)
+pause
+:namestarter2
+:nammee
+cls
+(
+echo New Pokemon Gained 
+echo What would you like to name %starter%?
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+::set /p startername=
+%pdest%\editvar64.exe -l 20 "startername"
+cls
+(
+echo You want it to be named "%startername%"?
+echo _._._._.y/n._._._._
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c yn /n >nul
+
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==2 (
+cls
+goto namestarter2
+)
+:town
+cls               
+
+(
+echo "%playername%" steps outside and looks at their home, Pallet Town, one more
+echo time before setting on an adventure. What will "%playername%" do?
+echo 1.Visit home       2.Start on route 1
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c 12 /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+goto home
+)
+if %errorlevel%==2 (
+cls
+goto route1
+)
+pause
+:home
+cls
+(
+echo "%playername%" goes to see their mother one last time, only to
+echo realize that their mother is still dead. After 5 years, you
+echo would think that a kid would remember being an orphan...
+echo Press any key to continue.
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+pause >nul
+cls
+goto town
+pause
+:route1
+cls
+(
+echo %playername% continues their journey with %startername% by their side.
+echo %playername% notices some rustling tall grass. What will %playername% do?
+echo 1.Investigate the tall grass	2.Go to Pallet Forest
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+choice /c 12 /n >nul
+start /min %pdest%\pokemonsounds\select.vbs
+if %errorlevel%==1 (
+cls
+goto route1ratata
+)
+if %errorlevel%==2 (
+cls
+goto palletforest
+)
+
+:route1ratata
+cls
+set /a battlepoke=%random%%%3
+if %battlepoke%==0 (
+type %pdest%\pokemonsounds\019.txt
+
+set enemytxt=%pdest%\pokemonsounds\019.txt
+set enemyname=Ratatta
+set enemymaxhp=25
+set enemyhp=25
+set enemyattack=12
+set enemydefence=10
+set enemyattack1=Bite
+set enemyattack2=Thunder
+set enemyattack3=Hyper Fang
+set enemyattack4=empty
+)
+if %battlepoke%==1 (
+type %pdest%\pokemonsounds\016.txt
+set enemytxt=%pdest%\pokemonsounds\016.txt
+set enemyname=Pidgey
+
+set enemymaxhp=25
+set enemyhp=25
+set enemyattack=12
+set enemydefence=10
+set enemyattack1=Tackle
+set enemyattack2=Quick Attack
+set enemyattack3=Aerial Ace
+set enemyattack4=Air Cutter
+)
+if %battlepoke%==2 (
+type %pdest%\pokemonsounds\013.txt
+set enemytxt=%pdest%\pokemonsounds\013.txt
+set enemyname=Weedle
+set enemymaxhp=50
+set enemyhp=50
+set enemyattack=12
+set enemydefence=10
+set enemyattack1=Tackle
+set enemyattack2=Quick Attack
+set enemyattack3=Aerial Ace
+set enemyattack4=Air Cutter
+)
+cls
+(						  
+echo Oh My! A %enemyname% Appeared 
+set redo=0
+
+
+:startbattle
+echo Go get em %startername% 
+
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+ping localhost -n 3 >nul
+cls
+
+
+
+set eminus=0
+set sminus=0
+tskill wscript
+start /min %pdest%\pokemonsounds\battle.vbs
+color 07
+:battle
+
+
+set /a epercent=%enemyhp%*100/%enemymaxhp%
+set /a spercent=%starterhp%*100/%startermaxhp%
+cls
+::enemy
+set ehpcolor=aa
+if %enemyhp% LEQ 15 set ehpcolor=ee
+if %enemyhp% LEQ 7 set ehpcolor=cc
+::starter
+
+set shpcolor=aa
+if %starterhp% LEQ 15 set shpcolor=ee
+if %starterhp% LEQ 7 set shpcolor=cc
+
+
+if %starterhp% LEQ 0 (
+goto lose
+)
+
+cls
+echo %enemyname%: %enemyhp%/%enemymaxhp%                                          %Startername%: %starterhp%/%startermaxhp%
+
+echo \----------------------------------------------------------------------------------------------------------------------   
+
+cd %pdest%\files\
+
+	(
+	Call loading 1 2 50 %ehpcolor% 4 1 NoFill %epercent%
+	%pdest%\files\batbox /w 100
+	)
+	
+	(
+	Call loading 55 2 50 %shpcolor% 4 1 NoFill %spercent%
+	%pdest%\files\batbox /w 100
+	)
+for /l %%A in (1, 1, 4) do echo(
+
+
+::colors for health bar
+::enemy
+set ehpcolor=aa
+if %enemyhp% LEQ 15 set ehpcolor=ee
+if %enemyhp% LEQ 7 set ehpcolor=cc
+::starter
+
+set shpcolor=aa
+if %starterhp% LEQ 15 set shpcolor=ee
+if %starterhp% LEQ 7 set shpcolor=cc::colors for health bar
+
+
+:chooseattack
+%userprofile%\pokebatch\DROPDOWNBOX "1: %attack[0]%;2: %attack[1]%;3: %attack[2]%" "Select An Attack" "Attack" /RI /C:157 >nul
+
+if %errorlevel%==0 goto chooseattack
+set starterattack=%errorlevel%
+
+
+
+set /a atsound=%random%%%3
+if %atsound%==0 (
+start /min %pdest%\pokemonsounds\att1.vbs
+)
+if %atsound%==1 (
+start /min %pdest%\pokemonsounds\att2.vbs
+)
+if %atsound%==2 (
+start /min %pdest%\pokemonsounds\att3.vbs
+)
+
+
+
+if %starterattack%==1 (
+set /a enemyhp-=2
+)
+if %starterattack%==2 (
+set /a enemyhp-=6
+)
+if %starterattack%==3 (
+set /a enemyhp-=4
+)
+if %starterattack%==4 (
+set /a enemyhp-=4
+)
+
+:battle2
+echo(
+
+
+echo %enemyname%: %enemyhp%/%enemymaxhp%                                          %Startername%: %starterhp%/%startermaxhp%
+
+echo \----------------------------------------------------------------------------------------------------------------------  
+
+cd %pdest%\files\
+
+
+	(
+	Call loading 1 2 50 %ehpcolor% 4 1 NoFill %epercent%
+	%pdest%\files\batbox /w 100
+	)
+	
+	(
+	Call loading 55 2 50 %shpcolor% 4 1 NoFill %spercent%
+	%pdest%\files\batbox /w 100
+	)
+
+cd %pdest%
+
+
+
+
+
+if %enemyhp% LEQ 0 (
+goto win
+)
+
+for /l %%A in (1, 1, 10) do echo(
+Echo                                Waiting for enemy move...
+ping localhost -n 2 >NUL
+
+set /a enemyattacknum=%random%%%4
+if %enemyattacknum%==0 (
+set sminus=2
+set enemyattack=Scratch
+set /a starterhp=%starterhp% -%sminus%
+)
+if %enemyattacknum%==1 (
+set sminus=6
+set enemyattack=%enemyattack1%
+set /a starterhp=%starterhp% -%sminus%
+)
+if %enemyattacknum%==2 (
+set sminus=4
+set enemyattack=%enemyattack2%
+set /a starterhp=%starterhp% -%sminus%
+)
+if %enemyattacknum%==3 (
+set sminus=5
+set enemyattack=%enemyattack3%
+set /a starterhp=%starterhp% -%sminus%
+)
+
+for /l %%A in (1, 1, 2) do echo(
+echo                                 %enemyname% used '%enemyattack%' 
+ping localhost -n 2 >nul
+
+
+
+goto battle
+
+
+
+
+
+
+
+
+
+
+
+:win
+cls
+
+(
+echo %enemyname%
+echo HP:%enemyhp%
+echo(
+echo %startername%
+echo HP:%starterhp%
+echo(
+Echo %startername% defeated %enemyname% 
+Echo You Win!
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+pause>nul
+exit
+
+:lose
+cls
+(
+echo %enemyname%
+echo HP:%enemyhp%
+echo(
+echo %startername%
+echo HP:%starterhp%
+echo(
+Echo %startername% defeated %enemyname% 
+Echo You Lose!
+)|%pdest%\boxes-1.2\boxes.exe -d stone -s 119x29 -a c
+pause>nul
+exit
+
+
+
+
+
+
+
+
+
